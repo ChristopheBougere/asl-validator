106324113d310453f1cd13afb2f1ce2a22c2e050
Christophe Bougère <cbougere@airware.com>
2018-05-03 14:42:55 +0200

    checking if states defined in StartAt, Next and Default exist

--- src/lib/missing-transition-target.js
+++ src/lib/missing-transition-target.js
@@ -14,9 +14,18 @@ module.exports = (definition) => {
     .filter((path, pos, array) => array.indexOf(path) === pos);
 
   // check if all states are reachable
-  return machineStates.filter(state => reachableStates.indexOf(state) === -1)
+  const unreachable = machineStates.filter(state => reachableStates.indexOf(state) === -1)
     .map(state => ({
       'Error code': 'MISSING_TRANSITION_TARGET',
       Message: `State ${state} is not reachable`,
     }));
+
+  // check if all 'Next', 'StartAt' and 'Default' states exist
+  const inexistant = reachableStates.filter(state => machineStates.indexOf(state) === -1)
+    .map(state => ({
+      'Error code': 'MISSING_TRANSITION_TARGET',
+      Message: `Missing 'StartAt'|'Next'|'Default' target: ${state}`,
+    }));
+
+  return unreachable.concat(inexistant);
 };
