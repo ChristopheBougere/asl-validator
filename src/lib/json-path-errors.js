7256b550ea1f2fc8daffab6706fd23fbfa95b306
Christophe Bougère <christophe.bougere@gmail.com>
2021-02-07 18:25:56 +0100

    Support object context

--- src/lib/json-path-errors.js
+++ src/lib/json-path-errors.js
@@ -3,6 +3,14 @@ const jp = require('jsonpath');
 module.exports = (definition) => jp.query(definition, '$..[\'InputPath\',\'OutputPath\',\'ResultPath\']')
   .filter((path) => typeof path === 'string')
   .map((path) => {
+    // Context object path starts with $$
+    // https://docs.aws.amazon.com/step-functions/latest/dg/input-output-contextobject.html#contextobject-access
+    if (path.startsWith('$$')) {
+      return path.substring(1);
+    }
+    return path;
+  })
+  .map((path) => {
     try {
       jp.parse(path);
       return null;
