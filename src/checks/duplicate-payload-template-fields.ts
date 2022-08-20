78fa32eb790c818fbee8720eecca1cd9559743fa
markford <mark@massfords.com>
2022-08-20 08:49:21 -0400

    fix(check): replace jsonpath-plus expression with traversal for dupe field check

--- src/checks/duplicate-payload-template-fields.ts
+++ src/checks/duplicate-payload-template-fields.ts
@@ -9,28 +9,12 @@ export const mustNotHaveDuplicateFieldNamesAfterEvaluation: AslChecker = (defini
     // for each one, examine each node's children for field names
     // that will be in conflict after evaluation.
     const errorMessages: StateMachineError[] = [];
-    let valuesToInspect: Record<string, unknown>[] = [];
-    try {
-        valuesToInspect = JSONPath<Record<string, unknown>[]>({
-            json: definition,
-            // this uses some extensions to the original JSONPath spec supported by the lib
-            //
-            // extension 1: @property.match enables matching on the matched property name.
-            //              This finds and selects all nodes with a name ending in .$
-            // extension 2: ^ operator twice at the end selects the parent node instead of the value for the node
-            //
-            // also, don't miss the array offset [0] before selecting the grandparent.
-            // There may be multiple fields within a node that could be in conflict.
-            // We're checking all the keys in the parent node, so we only need a single
-            // result for that parent.
-            path: `$..[Parameters,ResultSelector]..*[?(@property.match(/^.+\\.\\$$/))][0]^^`
-        })
-    } catch (err: unknown) {
-        // need to revisit this since the JSONPath lib throws an error when it encounters an array
-        // Error: jsonPath: _$_property.match is not a function: _$_property.match(/^.+\.\$$/)
-    }
+    const valuesToInspect: Record<string, unknown>[] = JSONPath<Record<string, unknown>[]>({
+        json: definition,
+        path: `$..[Parameters,ResultSelector]`
+    })
 
-    valuesToInspect.forEach((parent) => {
+    const inspectKeys = (parent: Record<string, unknown>): void => {
         const keys: Record<string, number> = {};
         Object.keys(parent).forEach((key) => {
             const keyPostEval = key.endsWith(".$") ? key.substring(0, key.length - 2) : key;
@@ -42,7 +26,12 @@ export const mustNotHaveDuplicateFieldNamesAfterEvaluation: AslChecker = (defini
                 });
             }
             keys[keyPostEval] = current ? current + 1 : 1;
+            if (typeof parent[key] === "object") {
+                inspectKeys(parent[key] as Record<string, unknown>);
+            }
         });
-    });
+    }
+
+    valuesToInspect.forEach((parent) => inspectKeys(parent));
     return errorMessages;
 }
