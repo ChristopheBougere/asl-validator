11f9427659c5b41832fadee32b17c8a1ca748ba7
Christophe Bougère <christophe.bougere@gmail.com>
2023-05-31 21:29:24 +0200

    feat: allow validating multiple definitions at once in the cli

--- bin/asl-validator.ts
+++ bin/asl-validator.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console, prefer-destructuring */
 
 import fs from 'fs';
 import program from 'commander';
@@ -7,51 +6,97 @@ import program from 'commander';
 import validator from '../src/validator';
 import {StateMachine, ValidationOptions} from '../src/types';
 
+enum ExitCode {
+  Success = 0,
+  ValidationError = 1,
+  ProgramError = 2,
+}
+
+function exit(exitCode: ExitCode) {
+  process.exit(exitCode);
+}
+
+function collect(value: string, previous: string[]) {
+  return previous.concat([value]);
+}
+
 program
   .description('Amazon States Language validator')
-  .option('--json-definition <jsonDefinition>', 'JSON definition')
-  .option('--json-path <jsonPath>', 'JSON path')
+  .option('--json-definition <jsonDefinition>', 'JSON definition', collect, [])
+  .option('--json-path <jsonPath>', 'JSON path', collect, [])
   .option('--silent', 'silent mode')
   .option('--no-path-check', 'skips checking path expressions')
   .option('--no-arn-check', 'skips the arn check for Resource values')
   .parse(process.argv);
 
-let definition;
-try {
-  if (typeof program.jsonDefinition === 'string') {
-    definition = JSON.parse(program.jsonDefinition) as StateMachine;
-  } else if (typeof program.jsonPath === 'string') {
-    definition = JSON.parse(fs.readFileSync(program.jsonPath).toString()) as StateMachine;
-  } else {
-    console.log('--json-definition or --json-path is required.');
-    program.help();
-  }
-} catch (e) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function log(...args: any[]) {
   if (!program.silent) {
-    console.error('Unable to read or parse state machine definition:', e);
+    // eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-argument
+    console.log(...args);
   }
-  process.exit(2);
 }
-try {
-  const validationOpts: ValidationOptions = {
-    checkArn: !program.noArnCheck,
-    checkPaths: !program.noPathCheck
-  }
-  const result = validator(definition, validationOpts);
-  if (result.isValid) {
-    if (!program.silent) {
-      console.log('✓ State machine definition is valid');
-    }
-    process.exit(0);
-  } else {
-    if (!program.silent) {
-      console.error('✕ State machine definition is invalid:\n', result.errorsText());
-    }
-    process.exit(1);
-  }
-} catch (e) {
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function error(...args: any[]) {
   if (!program.silent) {
-    console.error('Validator exception:', e);
+    // eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-argument
+    console.error(...args);
   }
-  process.exit(2);
 }
+
+function validate(name: string, definition: StateMachine) {
+  try {
+    const validationOpts: ValidationOptions = {
+      checkArn: !program.noArnCheck,
+      checkPaths: !program.noPathCheck
+    }
+    const result = validator(definition, validationOpts);
+    if (result.isValid) {
+      log(`✓ State machine definition "${name}" is valid`);
+      return ExitCode.Success;
+    }
+    error(`✕ State machine definition "${name}" is invalid:\n`, result.errorsText());
+    return ExitCode.ValidationError;
+  } catch (err) {
+    error('Validator exception:', err);
+    return ExitCode.ProgramError;
+  }
+}
+
+function main() {
+  try {
+    const jsonDefinitions = (program.jsonDefinition ?? []) as string[];
+    const jsonPaths = (program.jsonPath ?? []) as string[];
+    if (jsonDefinitions.length === 0 && jsonPaths.length === 0) {
+      log('--json-definition or --json-path is required.');
+      program.help();
+    }
+
+    let exitCode = ExitCode.Success;
+
+    // Validate JSON definitions
+    jsonDefinitions.forEach((jsonDefinition: string, index: number) => {
+      const name = `json definition #${index + 1}`;
+      let definition;
+      try {
+        definition = JSON.parse(jsonDefinition) as StateMachine;
+        exitCode = Math.max(validate(name, definition));
+      } catch (err) {
+        error(`Unable to parse ${name}`);
+      }
+    });
+
+    // Validate JSON paths
+    jsonPaths.forEach((jsonPath: string) => {
+      const definition = JSON.parse(fs.readFileSync(jsonPath).toString()) as StateMachine;
+      exitCode = Math.max(exitCode, validate(jsonPath, definition));
+    });
+    exit(exitCode);
+  } catch (err) {
+    error(err);
+    exit(ExitCode.ProgramError);
+  }
+}
+
+main();
