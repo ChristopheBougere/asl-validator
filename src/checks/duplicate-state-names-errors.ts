e2910cb54215506666f28352843b319f33e36f3e
Christophe Bougère <christophe.bougere@gmail.com>
2023-06-02 21:04:24 +0200

    chore: run prettier on all files

--- src/checks/duplicate-state-names-errors.ts
+++ src/checks/duplicate-state-names-errors.ts
@@ -1,24 +1,30 @@
-import {JSONPath} from 'jsonpath-plus';
-import {AslChecker, StateMachineError, StateMachineErrorCode, States} from '../types';
+import { JSONPath } from "jsonpath-plus";
+import {
+  AslChecker,
+  StateMachineError,
+  StateMachineErrorCode,
+  States,
+} from "../types";
 
 export const duplicateStateNames: AslChecker = (definition) => {
-    const errorMessages: StateMachineError[] = [];
-    const names: Record<string, number> = {};
-    JSONPath<States[]>({json: definition, path: '$..[\'States\']'})
-        .forEach((states) => {
-            Object.keys(states).forEach((stateName) => {
-                const current = names[stateName];
-                names[stateName] = current ? current + 1 : 1;
-            });
-        });
-    for (const [key, value] of Object.entries(names)) {
-        if (value > 1) {
-            errorMessages.push({
-                'Error code': StateMachineErrorCode.DuplicateStateNames,
-                Message: `A state with this name already exists: ${key}`,
-            });
-        }
+  const errorMessages: StateMachineError[] = [];
+  const names: Record<string, number> = {};
+  JSONPath<States[]>({ json: definition, path: "$..['States']" }).forEach(
+    (states) => {
+      Object.keys(states).forEach((stateName) => {
+        const current = names[stateName];
+        names[stateName] = current ? current + 1 : 1;
+      });
     }
+  );
+  for (const [key, value] of Object.entries(names)) {
+    if (value > 1) {
+      errorMessages.push({
+        "Error code": StateMachineErrorCode.DuplicateStateNames,
+        Message: `A state with this name already exists: ${key}`,
+      });
+    }
+  }
 
-    return errorMessages;
-}
+  return errorMessages;
+};
--- src/checks/formats.ts
+++ src/checks/formats.ts
@@ -1,6 +1,7 @@
 // spec is here: https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
-const arn = /^arn:(?<Partition>aws|aws-cn|aws-us-gov):(?<Service>[^:\n]+):(?<Region>[^:\n]*):(?<AccountID>\d*):(?<Resource>([^:/*]+([:/].*[^:/]+)?))$/ui
+const arn =
+  /^arn:(?<Partition>aws|aws-cn|aws-us-gov):(?<Service>[^:\n]+):(?<Region>[^:\n]*):(?<AccountID>\d*):(?<Resource>([^:/*]+([:/].*[^:/]+)?))$/iu;
 
-export const isArnFormatValid = (taskResource: string) : boolean => {
-    return arn.test(taskResource);
-}
+export const isArnFormatValid = (taskResource: string): boolean => {
+  return arn.test(taskResource);
+};
