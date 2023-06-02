fa6bd2d602331afd085bcfccda6d2e02ed12c54b
Christophe Bougère <christophe.bougere@gmail.com>
2023-06-02 21:31:53 +0200

    docs: add a changelog

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog"],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+      },
+    ],
+  ],
+};
