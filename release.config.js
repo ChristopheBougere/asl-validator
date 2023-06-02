5372e3aa5cd850540c18db50548f7d765a790c90
Christophe Bougère <christophe.bougere@gmail.com>
2023-06-02 22:00:11 +0200

    chore: remove changelog

--- release.config.js
+++ release.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   branches: ["main"],
   tagFormat: "${version}",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog"],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-      },
-    ],
-  ],
 };
